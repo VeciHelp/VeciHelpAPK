00f32913ef928002dd652e5de7e201e5bec23e49
Enzo Rojas <rojasjarpaenzo@hotmail.com>
2020-10-16 19:25:01 -0300

    Label Vecinos Asociados modificada altura.

--- VeciHelpAPK/VeciHelpAPK/Views/AlertasActivas.xaml.cs
+++ VeciHelpAPK/VeciHelpAPK/Views/AlertasActivas.xaml.cs
@@ -35,16 +35,22 @@ namespace VeciHelpAPK.Views
 
             var response = await endPoint.AlertasActivas(idUsuario);
 
+            if (response == null)
+            {
+                LabelAlertasActivas.Text = "Que bien, no hay alertas activas";
+                LabelAlertasActivas.FontAttributes = FontAttributes.Bold;
+                LabelAlertasActivas.TextColor = Color.White;
+            }
+
             foreach (var item in response)
             {
-                if(item == null)
+                if(response == null)
                 {
                     LabelAlertasActivas.Text = "Que bien, no hay alertas activas";
                     LabelAlertasActivas.FontAttributes = FontAttributes.Bold;
                     LabelAlertasActivas.TextColor = Color.White;
                 }
-                else
-                {
+               
                     Button btnAlertas = new Button();
                     btnAlertas.CornerRadius = 20;
                     btnAlertas.FontAttributes = FontAttributes.Bold;
@@ -72,7 +78,7 @@ namespace VeciHelpAPK.Views
                     btnAlertas.TextColor = Color.White;
 
                     StackAlertas.Children.Add(btnAlertas);
-                }
+                
 
                 
             }
